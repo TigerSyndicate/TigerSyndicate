17a6752f4fbf3f841737ae34ec013172fc1b08ec
edward <hughes_personal@yahoo.com>
2016-09-22 05:47:19 -0500

    init 2of?
    - web ready at around v4.x.x
    - splitting code upload into smaller sections
    - upload phps

---
+++ WebReady/footer.php
@@ -0,0 +1,33 @@
+<div class="col-12" id="footer">
+		<hr />
+		<!--Footer Icons-->
+		<ul id="bootstrap-icons">
+			<li><a href="https://discord.gg/0WJQe0bFBIxW0oES" target="_blank">
+				<div title="Discord" alt="Discord" id="discord"></div></a></li>
+			
+			<li><a href="https://www.facebook.com/TheTigerSyndicate" target="_blank">
+				<i class="fa fa-facebook-square" title="Facebook" alt="Facebook" id="facebook"></i></a></li>
+			
+			<li><a href="https://www.youtube.com/channel/UCg75nwF0i7_CljnWKnHsFzg" target="_blank">
+				<i class="fa fa-youtube-play" title="Youtube" alt="Youtube" id="youtube"></i></a></li>
+			
+			<li><a href="http://steamcommunity.com/groups/TigerSyndicate" target="_blank">
+				<i class="fa fa-steam-square" title="Steam" alt="Steam" id="steam"></i></a></li>
+			
+			<li><a href="https://plus.google.com/u/0/b/108420156032489249078/108420156032489249078/videos" target="_blank">
+				<i class="fa fa-google-plus-square" title="Google+" alt="Google+" id="googleplus"></i></a></li>
+			
+			<li><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=W5QEEKGGKUX3C" target="_blank">
+				<i class="fa fa-paypal" title="Paypal" alt="Paypal" id="paypal"></i></a></li>
+			
+			<li><a href="contacts.php">
+				<i class="fa fa-envelope-o" title="Contacts" alt="Contacts"></i></a></li>
+			
+			<li><a href="faq.php">
+				<i class="fa fa-question" title="FAQs" alt="FAQs"></i></a></li>
+		</ul>
+		<br />
+		&copy 2016 <a href="index.php">Tiger Syndicate</a>
+		<br />
+		<a href="https://www.edwardyauhughes.com">Web Designer</a>
+</div>
---
+++ WebReady/head.php
@@ -0,0 +1,15 @@
+<!--Meta Data-->
+<meta charset="UTF-8" />
+<meta name="author" content="Tiger Syndicate" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="keywords" content="Tiger, Syndicate, Tiger Syndicate, TGSYD, gaming community, Electronic Sports Team, E Sports, Video Game, gaming, gamer, tournament, events, Blade and Soul, Brawlhalla, Company of Heroes, Counter-Strike: Global Offensive, Dark Souls 3, Destiny, Dota 2, Hearth Stone, Heroes of the Storm, League of Legends, Overwatch, Starcraft 2, Street Fighter 5, Warframe, World of Warcraft" />
+<meta name="description" content="Tiger Syndicate is an up and coming gaming label. Our goal is to become a professional electronics sports team." />
+
+<!--Small Icon on Browser Tab-->
+<link type="image/x-icon" rel="shortcut icon" href="images/logos/TigerSyndicate_main_icon.png" />
+
+<!--bootstrapcdn; Icons-->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
+
+<!--Title-->
+<title>Tiger Syndicate | TGSYD</title>
