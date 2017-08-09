a8870abce1ec123bc4f844c7693ccac0a083e56c
Edward Hughes <hughes5683@gmail.com>
2017-08-09 14:34:14 +0000

    4.0

---
+++ 4.0/js/main.js
@@ -0,0 +1,29 @@
+$(function(){
+    //Toggle open nav panel
+    $("#nav, #exitNav").click(function(){
+        ToggleNavOverlay();
+    });
+    
+    //Nav links
+    /*
+    $(".navbtn").click(function(){
+        var clickedNavBtn = $(".navbtn:focus").data("page");
+        ToggleNavOverlay();
+        
+        if(clickedNavBtn === "teams"){
+            ListGames();
+        }
+        else if(clickedNavBtn === "merch"){
+            alert("not implemented");
+        }
+        else if(clickedNavBtn === "contact"){
+            DisplayContact();
+        }
+        else if(clickedNavBtn === "faq"){
+            DisplayFAQ();
+        }
+    });
+    */
+});
+
+    
