d3620e9949e23f2ab2a67b163077aa21e5d2694c
Edward Hughes <hughes5683@gmail.com>
2017-08-10 21:09:49 +0000

    push to master script

--- push-master.sh
+++ push-master.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
 git add .
+echo ""
 echo ">>>  GIT ADD ."
+echo ""
 
 git status
+echo ""
 echo ">>>  GIT STATUS"
+echo ""
 
 
 if [ "$#" -eq 0 ]
 then
     git commit -m "update"
+    echo ""
     echo ">>>  GIT COMMIT -M 'UPDATE'"
+    echo ""
     
 elif [ "$#" -gt 0 ]
 then
     git commit -m "$*"
+    echo ""
     echo ">>>  GIT COMMIT -M 'CMDLINE MSG'"
+    echo ""
 fi
 
 
 git push origin master
+echo ""
 echo ">>>  GIT PUSH ORIGIN MASTER"
+echo ""
