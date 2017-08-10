724b1f56f88b3bf8a0c79949c36c91419e861498
Edward Hughes <hughes5683@gmail.com>
2017-08-10 20:59:28 +0000

    -du

--- push-master.sh
+++ push-master.sh
@@ -1,34 +1,29 @@
 #!/bin/bash
 
 git add .
-echo ">>>  git add ."
+echo ">>>  GIT ADD."
 
 git status
-echo ">>>  git status"
+echo ">>>  GIT STATUS"
 
 
-if [ "$#" -eq 1 ]
+if [ "$#" -eq 0 ]
 then
     git commit -m "update"
-    echo ">>>  git commit -m 'update'"
+    echo ">>>  GIT COMMIT -M 'UPDATE'"
     
-    git push origin master
-    echo ">>>  git push origin master"
-    
-    if [ "$1" -eq "-du" ]
-    then
-        reddtoric
-    fi
-    
-elif [ "$#" -eq 2 ]
+elif [ "$#" -eq 1 ]
 then
     git commit -m $1
-    echo ">>>  git commit -m 'cmdline msg'"
+    echo ">>>  GIT COMMIT -M 'CMDLINE MSG'"
     
 else
     echo " "
-    echo ">>>  Illegal cmdline argument."
-    echo ">>>  ./push-master 'msg' 'username'"
-    echo ">>>  or ./push-master 'username'"
-    echo ">>>  or ./push-master -du (for default username)"
+    echo ">>>  ILLEGAL CMDLINE ARGUMENT."
+    echo ">>>  ./PUSH-MASTER"
+    echo ">>>  OR ./PUSH-MASTER 'MSG'"
 fi
+
+
+git push origin master
+echo ">>>  GIT PUSH ORIGIN MASTER"
