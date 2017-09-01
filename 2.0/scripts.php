bbaf14141616436366347af3ece0f12bc2a43c5f
Edward Hughes <hughes5683@gmail.com>
2017-09-01 02:02:43 +0000

    added deleted css rules that shouldnt have been deleted

--- 2.0/scripts.php
+++
@@ -1,6 +0,0 @@
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-<script type="text/javascript" src="http://www.bitstorm.org/jquery/color-animation/jquery.animate-colors-min.js"></script>
-<script type="text/javascript" src="https://raw.githubusercontent.com/edwinm/Color-animation-jQuery-plugin/master/jquery.animate-colors-min.js"></script>
-	<!-- ^color animation, only if available on web. otherwise from files [how to implement]-->
-	<!--why animate color from bitstorm work with chrome/ie but not github one?-->
-<script type="text/javascript" src="js/JScript.js"></script>
--- push-both.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#use only in grandmaster branch!
-
-echo ""
-echo ">>>  PUSHING TO GRANDMASTER AND MASTER"
-echo ""
-
-./push-grandmaster.sh $*
-./push-master.sh $*
